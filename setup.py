926bc79f31be6fa1a3d7acf95e88340a1ed0b581
Zhukov Pavel <gelios@gmail.com>
2011-06-30 11:03:16 +0400

    Correctly include locale

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 
 setup(
     name='django-shop-configurableproduct',
-    version='0.2.5',
+    version='0.2.6',
     packages=find_packages(),
     install_requires=[
         'sorl-thumbnail',
